f34f3b85c8d6569b148c80773e70cab095803c30
cringland <cringland@deloitte.co.uk>
2021-12-03 09:46:58 +0000

    Day 3

--- 2021/src/Main.kt
+++ 2021/src/Main.kt
@@ -1,9 +1,11 @@
 import day1.Day1
 import day2.Day2
+import day3.Day3
 
 fun main(args: Array<String>) {
-    logDay(Day1(), 1)
-    logDay(Day2(), 2)
+//    logDay(Day1(), 1)
+//    logDay(Day2(), 2)
+    logDay(Day3(), 3)
 }
 
 fun logDay(day: Day, i: Int) {
---
+++ 2021/src/day3/Day3.kt
@@ -0,0 +1,49 @@
+package day3
+
+import java.io.File
+import Day
+
+class Day3 : Day {
+    private val input = File("src/day3/input").readLines().filter { it.isNotEmpty() }
+            .map { it.toList().map(Character::getNumericValue) }
+
+    override fun problemOne(): String {
+        val maxBinary = input.columnCounts()
+                .map { map -> if (map[0].orEmpty().size > map[1].orEmpty().size) 0 else 1 }
+        return (maxBinary.binaryToDecimal() * maxBinary.invertBinary().binaryToDecimal()).toString()
+    }
+
+    override fun problemTwo(): String {
+        val oxygen = getMostLike { ones, zeros -> ones >= zeros}
+        val co2 = getMostLike { ones, zeros -> ones < zeros}
+        return (oxygen.binaryToDecimal() * co2.binaryToDecimal()).toString()
+    }
+    
+    fun getMostLike(onesToZerosCompare: (Int, Int) -> Boolean): List<Int> {
+        var list = input
+        var i = 0;
+        while (list.size > 1) {
+            val counts = list.columnCounts()
+            val num = if (onesToZerosCompare.invoke(counts[i][1].orEmpty().size, counts[i][0].orEmpty().size)) 1 else 0
+            list = list.filter { num == it[i] }
+            i++
+        }
+        return list[0]
+    }
+
+    fun List<List<Int>>.columnCounts(): List<Map<Int, List<Int>>> {
+        return this.first().mapIndexed { i, _ ->
+            this.map({ it[i] }).groupBy { it }
+        }
+    }
+
+    fun List<Int>.binaryToDecimal(): Int {
+        return this.reversed().reduceIndexed { i, sum, value ->
+            sum + (value * Math.pow(2.0, i.toDouble())).toInt()
+        }
+    }
+
+    fun List<Int>.invertBinary(): List<Int> {
+        return this.map { if (it == 1) 0 else 1 }
+    }
+}
