a8a49b2ceb3c563562c808ddf0552c58afc58285
cringland <cringland@deloitte.co.uk>
2023-12-05 11:19:34 +0000

    Adding day 5

--- 2023/src/Main.kt
+++ 2023/src/Main.kt
@@ -25,7 +25,7 @@ import day8.Day8
 import day9.Day9
 
 fun main() {
-    val currentDay = 4
+    val currentDay = 5
     val days = listOf(
         { Day1() },
         { Day2() },
--- 2023/src/day5/Day5.kt
+++ 2023/src/day5/Day5.kt
@@ -1,16 +1,55 @@
 package day5
 
 import Day
+import java.time.LocalTime
 import java.util.*
 
 class Day5 : Day {
-    private val input = inputFile().readLines()
+    data class Converter(val destRangeStart: Long, val sourceRageStart: Long, val rangeLength: Long) {
+        fun canConvert(start: Long) = (sourceRageStart until (sourceRageStart + rangeLength)).contains(start)
+        fun convert(start: Long) = start - sourceRageStart + destRangeStart
+    }
+
+    private val seeds = inputFile().readLines().first()
+        .substringAfter(": ")
+        .split(" ")
+        .map { it.toLong() }
+
+    private val maps = inputFile().readText().substringAfter("\n\n")
+        .split("\n\n")
+        .map { block ->
+            val lines = block.lines()
+            lines.drop(1).map {
+                val nums = it.split(" ")
+                Converter(nums[0].toLong(), nums[1].toLong(), nums[2].toLong())
+            }
+        }
 
     override fun problemOne(): Number {
-        return 1
+        val mappedSeeds = seeds.map { seed ->
+            maps.fold(seed) { num, converters ->
+                converters.find { converter -> converter.canConvert(num) }?.convert(num) ?: num
+            }
+        }
+        return mappedSeeds.min()!!
     }
 
     override fun problemTwo(): Number {
-        return 1
+        val ranges = seeds.chunked(2)
+            .map { it[0]..(it[0] + it[1]) }
+        val mappedSeeds = ranges.mapIndexed { i, range ->
+            println("${LocalTime.now()}: Onto range $i")
+            range.fold(Long.MAX_VALUE) { currentMin, seed ->
+                val prevRanges = ranges.subList(0, i)
+                val current = maps.fold(seed) { num, converters ->
+                    if (prevRanges.none { seed in it })
+                        converters.find { converter -> converter.canConvert(num) }?.convert(num) ?: num
+                    else Long.MAX_VALUE
+                }
+                current.coerceAtMost(currentMin)
+            }
+        }
+        //Takes 20 minutes. Could maybe be improved a bit
+        return mappedSeeds.min()!!
     }
 }
