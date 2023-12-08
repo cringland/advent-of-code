18539226b68aa0245984025d2473a7bdb8103ef8
cringland <cringland@deloitte.co.uk>
2023-12-08 10:08:12 +0000

    Day 8

--- 2023/src/day8/Day8.kt
+++ 2023/src/day8/Day8.kt
@@ -1,16 +1,51 @@
 package day8
 
 import Day
+import java.util.concurrent.locks.Condition
 import kotlin.math.max
 
 class Day8 : Day {
     private val input = inputFile().readLines()
+    private val instructions = input[0]
+    private val map = input.drop(2).map {
+        it.replace("[\\(\\) ]".toRegex(), "")
+    }.associate {
+        val key = it.substringBefore("=")
+        val values = it.substringAfter("=").split(",").let { vals -> vals[0] to vals[1] }
+        key to values
+    }
 
     override fun problemOne(): Number {
-        return 1
+        return solve("AAA") { it == "ZZZ" }
     }
 
     override fun problemTwo(): Number {
-        return 1
+        val values = map.keys.filter { it.endsWith('A') }
+            .map { startKey -> solve(startKey) { it.endsWith('Z') } }
+        return lcm(values)
+    }
+
+    private fun lcm(list: List<Long>): Long {
+        val largest = list.max()!!
+        var lcm = largest
+        while(true) {
+            if (list.all { (lcm % it) == 0L })
+                return lcm
+            lcm += largest
+        }
+    }
+
+    private fun solve(startKey: String, endCondition: (String) -> Boolean): Long {
+        var currentKey = startKey
+        var index = 0
+        var count = 0L
+        while (!endCondition(currentKey)) {
+            if (index == instructions.length) index = 0
+            currentKey = if (instructions[index] == 'R') map[currentKey]!!.second
+            else map[currentKey]!!.first
+            count++
+            index++
+        }
+        return count
     }
 }
