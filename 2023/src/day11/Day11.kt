a5c5076b8f4d2dad35cee98f78ea219a3dc470a0
cringland <cringland@deloitte.co.uk>
2023-12-11 09:39:36 +0000

    Day 11

--- 2023/src/day11/Day11.kt
+++ 2023/src/day11/Day11.kt
@@ -1,15 +1,41 @@
 package day11
 
 import Day
+import util.Point2
+import util.by
+import kotlin.math.abs
+
+typealias InputGrid = List<String>
 
 class Day11 : Day {
     private val input = inputFile().readLines()
-
-    override fun problemOne(): Number {
-        return 1
+    private val expandedY = input.mapIndexedNotNull { y, it -> if (it.contains("#")) null else y }
+    private val expandedX = input.first().mapIndexedNotNull { x, _ ->
+        if (input.none { line2 -> line2[x] == '#' }) x else null
     }
+    private val ptPairs = input.mapIndexed { y, line ->
+        line.mapIndexedNotNull { x, c ->
+            if (c == '#') x by y else null
+        }
+    }.flatten()
+        .let { pts ->
+            pts.mapIndexed { i, p1 ->
+                pts.drop(i + 1).map { p2 -> p1 to p2 }
+            }.flatten()
+        }
 
-    override fun problemTwo(): Number {
-        return 1
-    }
+    private fun solve(multiplier: Long) =
+        ptPairs.map { pair ->
+            val p1 = pair.first
+            val p2 = pair.second
+            val diff = p1 - p2
+            val xRange = p1.x.coerceAtMost(p2.x)..p1.x.coerceAtLeast(p2.x)
+            val yRange = p1.y.coerceAtMost(p2.y)..p1.y.coerceAtLeast(p2.y)
+            val yCount = expandedY.count { it in yRange }
+            val xCount = expandedX.count { it in xRange }
+            abs(diff.x) + abs(diff.y) + (yCount.toLong() * multiplier) + (xCount.toLong() * multiplier)
+        }.sum()
+
+    override fun problemOne() = solve(1)
+    override fun problemTwo() = solve(999999)
 }
